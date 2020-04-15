50433fc6b0c64c700ce7a6408929fe3ec0dfb185
Vicky <ying.vicky.cao@outlook.com>
2020-04-15 16:46:05 +0800

    Duck user inheritance

---
+++ src/main/java/com/hades/example/dp/strategy_patttern/_1_Inheritance/MallardDuck.java
@@ -0,0 +1,11 @@
+package com.hades.example.dp.strategy_patttern._1_Inheritance;
+
+/**
+ * 绿头鸭
+ */
+public class MallardDuck extends Duck {
+    @Override
+    public void display() {
+        System.out.println("绿头");
+    }
+}
---
+++ src/main/java/com/hades/example/dp/strategy_patttern/_1_Inheritance/ReadHeadDuck.java
@@ -0,0 +1,11 @@
+package com.hades.example.dp.strategy_patttern._1_Inheritance;
+
+/**
+ * 红头鸭
+ */
+public class ReadHeadDuck extends Duck{
+    @Override
+    public void display() {
+        System.out.println("红头");
+    }
+}
