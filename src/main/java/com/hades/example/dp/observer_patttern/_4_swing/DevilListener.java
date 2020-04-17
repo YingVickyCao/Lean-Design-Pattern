28d1413517458a747b6877b1c3aeb244ab992570
Vicky <ying.vicky.cao@outlook.com>
2020-04-17 11:50:23 +0800

    观察者模式

---
+++ src/main/java/com/hades/example/dp/observer_patttern/_4_swing/DevilListener.java
@@ -0,0 +1,14 @@
+package com.hades.example.dp.observer_patttern._4_swing;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * 观察者：恶魔
+ */
+public class DevilListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Come on, do it!");
+    }
+}
