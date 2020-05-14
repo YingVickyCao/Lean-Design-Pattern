9a4548ae4b42fa17566cae6592917d55ce7be5f5
Vicky <ying.vicky.cao@outlook.com>
2020-05-14 10:31:23 +0800

    refactor

--- src/main/java/com/hades/example/designpatterns/composite/_2/CompositeIterator.java
+++ src/main/java/com/hades/example/designpatterns/composite/_2/CompositeIterator.java
@@ -6,6 +6,9 @@ import java.util.Stack;
 public class CompositeIterator implements Iterator {
     private Stack mStack = new Stack();
 
+    /**
+     * 将组合的迭代器传入
+     */
     public CompositeIterator(Iterator iterator) {
         mStack.push(iterator);
     }
@@ -16,6 +19,7 @@ public class CompositeIterator implements Iterator {
             return false;
         } else {
             Iterator iterator = (Iterator) mStack.peek();
+            // 没有下一个了，则弹出栈，然后递归地调用hasNext
             if (!iterator.hasNext()) {
                 mStack.pop();
                 return hasNext();
@@ -27,9 +31,11 @@ public class CompositeIterator implements Iterator {
 
     @Override
     public Object next() {
+        // 当客户要取得下一个元素时，先调用hasNext()来确定是否还有下一个
         if (hasNext()) {
             Iterator iterator = (Iterator) mStack.peek();
             MenuComponent menuComponent = (MenuComponent) iterator.next();
+            // 如果元素是个菜单，也是需要被包含进遍历的组合
             if (menuComponent instanceof Menu) {
                 mStack.push(menuComponent.createIterator());
             }
