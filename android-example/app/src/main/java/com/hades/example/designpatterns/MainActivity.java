173d599d340f6a094ec248a69d677f96b8344d4c
YingVickyCao <yingvickycao@163.com>
2025-01-24 15:45:05 +0800

    refactor

--- android-example/app/src/main/java/com/hades/example/designpatterns/MainActivity.java
+++ android-example/app/src/main/java/com/hades/example/designpatterns/MainActivity.java
@@ -1,16 +1,23 @@
 package com.hades.example.designpatterns;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+import com.hades.example.designpatterns.mvp.v.MVPExampleActivity;
+import com.hades.example.designpatterns.mvvm.MVVMExampleActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.mvc).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, com.hades.example.designpatterns.mvc.v1.c.MVCExampleActivity.class)));
+        findViewById(R.id.mvc2).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, com.hades.example.designpatterns.mvc.v2.v.MVCExampleActivity.class)));
+        findViewById(R.id.mvp).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MVPExampleActivity.class)));
+        findViewById(R.id.mvvc).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, MVVMExampleActivity.class)));
     }
-
 }
